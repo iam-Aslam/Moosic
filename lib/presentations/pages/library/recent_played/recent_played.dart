c9dfec3dbd0ff4b8e61c3d444327dbe0199f31dc
Mohammed Aslam M K <117348902+iam-Aslam@users.noreply.github.com>
2023-08-03 21:46:05 +0530

    code optimized

--- lib/presentations/pages/library/recent_played/recent_played.dart
+++ lib/presentations/pages/library/recent_played/recent_played.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
